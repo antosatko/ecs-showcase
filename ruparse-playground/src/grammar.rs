2aa385ba7974bde6283a745de895af0bb65a1dc1
it-2001 <it2001@sspu-opava.cz>
2025-12-29 02:46:21 +0100

    new ruparse interface

--- ruparse-playground/src/grammar.rs
+++ ruparse-playground/src/grammar.rs
@@ -1,11 +1,10 @@
 use ruparse::Parser;
 use ruparse::api::ext::*;
 use ruparse::grammar::validator::Validator;
-use ruparse::grammar::{Enumerator, MatchToken, Node, VarKind, VariableKind};
+use ruparse::grammar::{MatchToken, VarKind, VariableKind};
 
 const IDENTIFIER: VarKind<'static> = local("identifier");
 const IDENTIFIER_VAR: (&'static str, VariableKind) = ("identifier", VariableKind::Node);
-const END_STATEMATE: MatchToken<'static> = MatchToken::Node("end statemate");
 
 const KEYWORDS: &[&'static str] = &[
     "systems",
@@ -31,89 +30,82 @@ pub fn gen_parser() -> Parser<'static> {
         .lexer
         .add_tokens("+-*/;\"':({[<>]})|&!?=".split("").filter(|s| !s.is_empty()));
 
-    parser.grammar.add_enum(Enumerator {
-        name: "keywords",
-        values: KEYWORDS.iter().map(|kw| word(kw)).collect(),
-    });
+    let keywords = parser
+        .grammar
+        .new_enum("keywords")
+        .options(KEYWORDS.iter().map(|kw| word(kw)))
+        .build();
 
-    parser
+    let ident = parser
         .grammar
         .new_node("identifier")
-        .rules([isnt(enumerator("keywords")), is(text()).set(IDENTIFIER)])
+        .rules([
+            isnt(keywords).hint("Keywords are reserved and can not be used for identifiers"),
+            is(text()).set(IDENTIFIER),
+        ])
         .variables([node_var("identifier")])
         .build();
 
-    parser.grammar.add_node(Node {
-        name: "identifier",
-        rules: rules([isnt(enumerator("keywords")), is(text()).set(IDENTIFIER)]),
-        variables: [("identifier", VariableKind::Node)].to_vec(),
-        docs: None,
-    });
+    let end_stmt = parser
+        .grammar
+        .new_node("end statement")
+        .rules([is_one_of([option(token(";")), option(newline())])
+            .hint("Expected to end statement on a new line or semicolon")])
+        .build();
 
-    parser.grammar.add_node(Node {
-        name: "end statement",
-        rules: rules([is_one_of(options([option(token(";")), option(newline())]))]),
-        variables: [].to_vec(),
-        docs: None,
-    });
-
-    parser.grammar.add_node(Node {
-        name: "systems",
-        rules: rules([
+    let systems = parser
+        .grammar
+        .new_node("systems")
+        .rules([
             is(keyword("systems")).commit(),
             is(token("{")),
             while_(node("identifier")).set(local("systems")),
             is(token("}")),
-        ]),
-        variables: [("systems", VariableKind::NodeList)].to_vec(),
-        docs: None,
-    });
+        ])
+        .variables([list_var("systems")])
+        .build();
 
-    parser.grammar.add_node(Node {
-        name: "resources",
-        rules: rules([
+    let resources = parser
+        .grammar
+        .new_node("resources")
+        .rules([
             is(keyword("resources")).commit(),
             is(token("{")),
             // while_(text()).set(local("resources")),
             is(token("}")),
-        ]),
-        variables: [("resources", VariableKind::NodeList)].to_vec(),
-        docs: None,
-    });
+        ])
+        .variables([list_var("resources")])
+        .build();
 
-    parser.grammar.add_node(Node {
-        name: "scheduler",
-        rules: rules([
+    let scheduler = parser
+        .grammar
+        .new_node("scheduler")
+        .rules([
             is(keyword("scheduler")).commit(),
-            is(node("identifier")).set(IDENTIFIER),
+            is(ident.clone()).set(IDENTIFIER),
             is(token("{")),
-            maybe(node("resources")).set(local("resources")),
-            maybe(node("systems")).set(local("systems")),
+            maybe(resources).set(local("resources")),
+            maybe(systems).set(local("systems")),
             is(token("}")),
-        ]),
-        variables: [
-            IDENTIFIER_VAR,
-            ("resources", VariableKind::Node),
-            ("systems", VariableKind::Node),
-        ]
-        .to_vec(),
-        docs: None,
-    });
+        ])
+        .variables([IDENTIFIER_VAR, node_var("resources"), node_var("systems")])
+        .build();
 
-    parser.grammar.add_enum(Enumerator {
-        name: "top level statements",
-        values: [node("scheduler")].to_vec(),
-    });
+    let tls = parser
+        .grammar
+        .new_enum("top level statements")
+        .options([scheduler])
+        .build();
 
-    parser.grammar.add_node(Node {
-        name: "entry",
-        rules: rules([loop_().then([is_one_of(options([
-            option(enumerator("top level statements")).set(local("top level statements")),
+    parser
+        .grammar
+        .new_node("entry")
+        .rules([loop_().then([is_one_of([
+            option(tls).set(local("top level statements")),
             option(eof()).return_node(),
-        ]))])]),
-        variables: [("top level statements", VariableKind::NodeList)].to_vec(),
-        docs: None,
-    });
+        ])])])
+        .variables([list_var("top level statements")])
+        .build();
     parser.parser.entry = Some("entry");
 
     let valid_result = Validator::default().validate(&parser);
